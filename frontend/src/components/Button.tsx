dd1d79bef63ae393158b0457b035e86e650d68ba
frankfang <frankfang1990@gmail.com>
2022-10-05 00:02:18 +0800

    编辑模式

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,15 +2,34 @@ import type { ButtonHTMLAttributes, ReactNode } from 'react'
 
 type ButtonProps = {
   children: ReactNode
+  size?: 'small' | 'medium' | 'large'
 } & ButtonHTMLAttributes<HTMLButtonElement>
 export const Button: React.FC<ButtonProps> = props => {
-  const { children, ...rest } = props
-  return (
-    <button type="button" h-40px bg-blue text-white pl-4 pr-4 b-rounded-1
-      disabled-bg-gray
-      {...rest}
-    >
-      {children}
-    </button>
-  )
+  const { children, size, ...rest } = props
+  switch (size) {
+    case 'small':
+      return (
+        <button type="button" h-24px bg-blue text-white pl-2 pr-2 b-rounded-1 disabled-bg-gray {...rest}>
+          {children}
+        </button>
+      )
+    case 'medium':
+      return (
+        <button type="button" h-40px bg-blue text-white pl-4 pr-4 b-rounded-1 disabled-bg-gray {...rest}>
+          {children}
+        </button>
+      )
+    case 'large':
+      return (
+        <button type="button" h-64px bg-blue text-white pl-4 pr-4 b-rounded-1 disabled-bg-gray {...rest}>
+          {children}
+        </button>
+      )
+    default:
+      return null
+  }
+}
+
+Button.defaultProps = {
+  size: 'medium',
 }
--- frontend/src/views/home/BookList.tsx
+++ frontend/src/views/home/BookList.tsx
@@ -1,9 +1,20 @@
 import dayjs from 'dayjs'
 import * as React from 'react'
+import { createContext, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useToggle } from 'react-use'
 import type { db } from '../../../wailsjs/go/models'
+import { Button } from '../../components/Button'
 import { useBooks } from '../../stores/useBooks'
+
+const EditModeContext = createContext<{
+  editMode: boolean
+  toggleEditMode: () => void
+}>({
+      editMode: false,
+      toggleEditMode: () => { throw new Error('not implemented') },
+    })
+
 export const BookList: React.FC = () => {
   const [editMode, toggleEditMode] = useToggle(false)
   const { books } = useBooks()
@@ -12,17 +23,24 @@ export const BookList: React.FC = () => {
       ? <section mt-16>
           <header flex justify-between border-b-1 b-gray-200 pb-2 pt-2>
             <h3 text-2xl>书籍列表</h3>
-            <button color-blue onClick={() => { toggleEditMode(true) }} >
+            {editMode
+              ? <button color-blue onClick={() => { toggleEditMode(false) }} >
+              退出编辑
+            </button>
+              : <button color-blue onClick={() => { toggleEditMode(true) }} >
               编辑
             </button>
+            }
           </header>
-          <ol flex flex-wrap gap-x-8 mt-4>
-            {books.map(b => (
-              <li w-16em key={b.id}>
-                <Card book={b} />
-              </li>
-            ))}
-          </ol>
+          <EditModeContext.Provider value={{ editMode, toggleEditMode }}>
+            <ol flex flex-wrap gap-x-8 mt-4>
+              {books.map(b => (
+                <li w-16em key={b.id}>
+                  <Card book={b} />
+                </li>
+              ))}
+            </ol>
+          </EditModeContext.Provider>
         </section>
       : null
   )
@@ -37,6 +55,7 @@ const Card: React.FC<CardProps> = ({ book }) => {
   const onClick = () => {
     nav(`/books/${book.id}/edit`)
   }
+  const c = useContext(EditModeContext)
   return (
     <div shadow bg-white p-4 mt-4 hover-shadow-xl onClick={onClick}>
       <h2 text-2xl overflow-hidden>
@@ -45,7 +64,9 @@ const Card: React.FC<CardProps> = ({ book }) => {
       <p h-3em text-gray line-clamp-2 mt-2 mb-2>
         {book.summary?.substring?.(0, 100)}
       </p>
-      <p color-gray>{dayjs(book.created_at).format('YYYY-MM-DD HH:mm')}</p>
+      <p color-gray flex justify-between items-center>{dayjs(book.created_at).format('YYYY-MM-DD HH:mm')}
+      { c.editMode && <Button size="small">删除</Button> }
+      </p>
     </div>
   )
 }
